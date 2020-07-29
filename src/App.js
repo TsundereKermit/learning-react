36dbd947ed8974d2ba670028dde9bd3a56690357
TsundereKermit <tonyjiang917@gmail.com>
2020-07-29 13:18:53 -0400

    added sound effects to the timer

--- src/App.js
+++ src/App.js
@@ -12,15 +12,20 @@ class App extends Component {
             second: 0, 
             err_reason: '',
             element: '',
+            sfx: '',
+            timerStarted: false,
+            url: '',
         };
     }
 
-    timerSubmit = (hour, minute, second) => {
+    timerSubmit = (hour, minute, second, sfx, url) => {
         if (hour < 0 || minute < 0 || second < 0) {
             this.setState({
                 hour: 0,
                 minute: 0,
                 second: 0,
+                sfx: '',
+                timerStarted: false,
                 err_reason: "Due to special relativity, negative time is not accepted as of this moment.",
             });
         } else if (minute > 59 || second > 60) {
@@ -28,14 +33,19 @@ class App extends Component {
                 hour: 0,
                 minute: 0,
                 second: 0,
+                sfx: '',
+                timerStarted: false,
                 err_reason: "Becaused we're civilized, we use the international standard notation for time here.",
             });
         } else {
             this.setState({
                 hour: hour, 
                 minute: minute, 
-                second: second, 
+                second: second,
+                sfx: sfx, 
+                timerStarted: true,
                 err_reason: "",
+                url: url,
             });
         }
     }
@@ -74,6 +84,41 @@ class App extends Component {
                 minute: 59,
                 second: 59,
             });
+        }
+
+        if (hour === 0 && minute === 0 && second === 0 && this.state.timerStarted) {
+            var sfxString = '';
+            if (this.state.sfx === "bruh sound effect 2") {
+                sfxString = "https://www.myinstants.com/media/sounds/movie_1.mp3";
+            } else if (this.state.sfx === "how dare you") {
+                sfxString = "https://www.myinstants.com/media/sounds/howdareyou_1GuCHbh.mp3"
+            } else if (this.state.sfx === "hello there") {
+                sfxString = "https://www.myinstants.com/media/sounds/obi-wan-hello-there.mp3";
+            } else if (this.state.sfx === "custom") {
+                sfxString = this.state.url;
+            }
+
+            let audio = new Audio(sfxString);
+            let audioPromise = audio.play();
+
+            if (audioPromise !== undefined) {
+                audioPromise
+                .then(() => {
+                    this.setState({
+                        sfx: '',
+                        timerStarted: false,
+                        url: '',
+                    })
+                })
+                .catch(err => 
+                    this.setState({
+                        sfx: '',
+                        timerStarted: false,
+                        url: '',
+                        err_reason: "Like, enter a valid sound URL or something. You're such a slob, b-baka!"
+                    })
+                );
+            }
         }
     }
     
--- src/TimerForm.js
+++ src/TimerForm.js
@@ -6,6 +6,9 @@ class TimerForm extends Component {
         hour: 0,
         minute: 0,
         second: 0,
+        sfx: "bruh sound effect 2",
+        custom: false,
+        url: '',
     }
  
     constructor(props) {
@@ -22,7 +25,7 @@ class TimerForm extends Component {
     }
 
     submitTimerForm = () => {
-        this.props.timerSubmit(this.state.hour, this.state.minute, this.state.second);
+        this.props.timerSubmit(this.state.hour, this.state.minute, this.state.second, this.state.sfx, this.state.url);
         this.setState(this.initialState);
     }
 
@@ -31,49 +34,46 @@ class TimerForm extends Component {
         this.props.timerSubmit(0,0,0);
     }
 
+    sfxChange = event => {
+        if (event.target.value !== "custom") {
+            this.setState({sfx: event.target.value});
+        } else {
+            this.setState({
+                custom: true,
+                sfx: event.target.value
+            });
+        }
+    }
+
+    customSfx = () => {
+        this.setState({custom: true});
+    }
+
     render() {
-        const {hour, minute, second} = this.state;
+        const {hour, minute, second, url} = this.state;
 
         return (
             <form id="timerForm">
                 <label htmlFor="hour">Hours</label>
-                <input 
-                    type="number"
-                    className="form-control mb-2"
-                    name="hour"
-                    id="hour"
-                    value={hour} 
-                    onChange={this.handleChange} />
+                <input type="number" className="form-control mb-2" name="hour" id="hour" value={hour} onChange={this.handleChange} />
                 <label htmlFor="minute">Minutes</label>
-                <input 
-                    type="number"
-                    className="form-control mb-2"
-                    name="minute"
-                    id="minute"
-                    value={minute} 
-                    onChange={this.handleChange} />
+                <input type="number" className="form-control mb-2" name="minute" id="minute" value={minute} onChange={this.handleChange} />
                 <label htmlFor="second">Seconds</label>
-                <input 
-                    type="number"
-                    className="form-control"
-                    name="second"
-                    id="second"
-                    value={second} 
-                    onChange={this.handleChange} />
+                <input type="number" className="form-control mb-2" name="second" id="second" value={second} onChange={this.handleChange} />
+                <label htmlFor="sfx">Sound effect</label>
+                <select className="form-control mb-2" value={this.state.sfx} onChange={this.sfxChange}>
+                    <option value="bruh sound effect 2">bruh sound effect 2</option>
+                    <option value="how dare you">how dare you</option>
+                    <option value="hello there">hello there</option>
+                    <option value="custom">custom</option>
+                </select>
+                {this.state.custom && 
+                <input className="form-control" type="url" placeholder="Enter your sound effect's URL here" id="url" name="url" value={url} onChange={this.handleChange} />}
                 <div className="divider-v"></div>
                 <div>
-                    <input 
-                        type="button"
-                        className="btn btn-primary"
-                        value="Start timer"
-                        form="timerForm"
-                        onClick={this.submitTimerForm} />
+                    <input type="button" className="btn btn-primary" value="Start timer" form="timerForm" onClick={this.submitTimerForm} />
                     <div className="divider-h"></div>
-                    <input 
-                        type="button" 
-                        className="btn btn-secondary"
-                        value="Reset"
-                        onClick={this.resetTimer} />
+                    <input type="button" className="btn btn-secondary" value="Reset" onClick={this.resetTimer} />
                 </div>
             </form>
         )
